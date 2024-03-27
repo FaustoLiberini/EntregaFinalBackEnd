349ccf405504609af2e77180d0d4f4177e22ca2c
FaustoLiberini <fau.liberini@gmail.com>
2024-03-26 21:26:59 -0300

    RefinandoLaEntrega

--- clinicaOdontologica/src/main/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ clinicaOdontologica/src/main/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
@@ -18,25 +16,8 @@ public class ClinicaOdontologicaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicaOdontologicaApplication.class, args);
-        crearTabla();
+        LOGGER.info("ClinicaOdontologica está corriendo...");
 
-    }
-
-    private static void crearTabla() {
-        Connection connection = null;
-        try {
-            Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection("jdbc:h2:~/clinicaOdontologia2;INIT=RUNSCRIPT FROM 'create.sql'", "a", "a");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
     }
 
     @Bean
